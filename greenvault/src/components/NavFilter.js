278243fc709ba2a40724d11a0f2c441951343d11
MCOCO <mathis.coco@ynov.com>
2023-04-16 20:50:05 +0200

    create article button conditional rendering

--- greenvault/src/components/NavFilter.js
+++ greenvault/src/components/NavFilter.js
@@ -2,20 +2,26 @@ import React from "react";
 import Navbar from "./Navbar.js";
 import Filters from "./Filters.js";
 import '../css/Topnav.css'
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useCookies } from "react-cookie";
 
 const NavFilter = () => {
     const navigate = useNavigate();
+    const location = useLocation();
+    const [cookies] = useCookies(['access_token']);
+    const isLoggedIn = cookies.access_token !== undefined;
 
     return (
         <div>
             <Navbar></Navbar>
             <Filters></Filters>
+            {location.pathname !== 'create_article' && isLoggedIn ? (
             <div className="add-product-btn">
                 <button onClick={() => navigate("/create_article")}>
                     <span className="plus-icon">+</span>
                 </button>
             </div>
+            ) : (<></>)}
         </div>
     )
 }
